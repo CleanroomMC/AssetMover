0b7bb2dc0069692fcfb4a50c78de8b26e19d193c
Rongmario <rongmario@protonmail.com>
2022-01-09 13:47:28 +0000

    Fixed overly-zealous duplicates checks

--- src/main/java/io/github/cleanroommc/assetmover/AssetMoverAPI.java
+++ src/main/java/io/github/cleanroommc/assetmover/AssetMoverAPI.java
@@ -25,22 +25,23 @@ public class AssetMoverAPI {
     }
 
     private static final String curseUrl = "https://addons-ecs.forgesvc.net/api/v2/addon/%s/file/%s";
+    private static final String mcAssetRepoUrl = "https://github.com/InventivetalentDev/minecraft-assets/raw/%s/%s";
     private static final Path parentPath = FMLLaunchHandler.isDeobfuscatedEnvironment() ? Paths.get("").resolve("assetmover") : Launch.minecraftHome.toPath().resolve("assetmover");
 
     private static final Map<String, Path> mcFiles = new Object2ObjectOpenHashMap<>();
     private static final Map<String, Path> urlFiles = new Object2ObjectOpenHashMap<>();
 
     public static void fromMinecraft(String mcVersion, Map<String, String> assets) {
-        if (hasDuplicates(assets)) {
-            return;
-        }
         for (Map.Entry<String, String> entry : assets.entrySet()) {
-            String source = entry.getKey();
             String target = entry.getValue();
+            if (!needsUpdating(target)) {
+                continue;
+            }
+            String source = entry.getKey();
             Path existingPath = mcFiles.get(source);
             if (existingPath == null) {
                 try {
-                    String urlString = "https://github.com/InventivetalentDev/minecraft-assets/raw/" + mcVersion + "/" + source;
+                    String urlString = String.format(mcAssetRepoUrl, mcVersion, source);
                     URL url = new URL(urlString);
                     Path targetPath = parentPath.resolve(target);
                     download(url, targetPath);
@@ -56,7 +57,7 @@ public class AssetMoverAPI {
 
     @UnstableApi
     public static void fromCurseForgeMod(String projectId, String fileId, Map<String, String> assets) {
-        if (hasDuplicates(assets)) {
+        if (!needsUpdating(assets)) {
             return;
         }
         try {
@@ -71,17 +72,18 @@ public class AssetMoverAPI {
     }
 
     public static void fromURL(URL url, String name, Map<String, String> assets) {
-        if (hasDuplicates(assets)) {
+        if (!needsUpdating(assets)) {
             return;
         }
         $fromURL(url, name, assets);
     }
 
-    private static boolean hasDuplicates(Map<String, String> assets) {
-        return assets.values().stream().anyMatch(s -> {
-            Path resolved = parentPath.resolve(s);
-            return Files.exists(resolved) && !Files.isDirectory(resolved);
-        });
+    private static boolean needsUpdating(String asset) {
+        return !Files.exists(parentPath.resolve(asset));
+    }
+
+    private static boolean needsUpdating(Map<String, String> asset) {
+        return asset.values().stream().anyMatch(s -> !Files.exists(parentPath.resolve(s)));
     }
 
     private static void $fromURL(URL url, String name, Map<String, String> assets) {
