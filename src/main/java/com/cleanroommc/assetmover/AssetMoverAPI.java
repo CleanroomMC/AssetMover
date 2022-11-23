76308e6c742372941c90664a449b79242271a833
Rongmario <rongmario@protonmail.com>
2022-11-23 11:59:36 +0000

    Bump to 2.1

--- src/main/java/com/cleanroommc/assetmover/AssetMoverAPI.java
+++ src/main/java/com/cleanroommc/assetmover/AssetMoverAPI.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class AssetMoverAPI {
 
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.1";
 
     static final Logger LOGGER = LogManager.getLogger("AssetMover");
     static final Path PARENT_PATH = Paths.get("").resolve("assetmover");
