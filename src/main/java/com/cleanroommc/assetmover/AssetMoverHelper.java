7cc30e15754466827d87a63626c01f48145b7829
Rongmario <rongmario@protonmail.com>
2023-02-17 11:55:36 +0000

    Fixes #5
    - Doesn't crash, and provides slightly better logging

--- src/main/java/com/cleanroommc/assetmover/AssetMoverHelper.java
+++ src/main/java/com/cleanroommc/assetmover/AssetMoverHelper.java
@@ -119,9 +119,11 @@ public enum AssetMoverHelper {
                 try (BufferedReader br = new BufferedReader(new FileReader(versionFile))) {
                     versionObject = new JsonParser().parse(br).getAsJsonObject();
                 } catch (IOException e) {
-                    AssetMoverAPI.LOGGER.fatal("Unexpected error occurred while reading {} {}", version + ".json", e);
+                    AssetMoverAPI.LOGGER.fatal("Unexpected error occurred while reading {}", version + ".json");
+                    AssetMoverAPI.LOGGER.fatal("Attempting to Re-Download {} from Mojang", version + ".json", e);
                 }
-            } else {
+            }
+            if (versionObject == null) {
                 if (VERSIONS_ARRAY == null) {
                     long start = System.currentTimeMillis();
                     URL manifestUrl = new URL(VERSIONS_MANIFEST);
@@ -134,33 +136,38 @@ public enum AssetMoverHelper {
                         AssetMoverAPI.LOGGER.fatal("Unexpected error occurred while downloading versions manifest", e);
                     }
                 }
-                URL versionUrl = null;
-                for (JsonElement versionElement : VERSIONS_ARRAY) {
-                    if (versionElement instanceof JsonObject) {
-                        JsonObject versionObj = versionElement.getAsJsonObject();
-                        String id = versionObj.get("id").getAsString();
-                        if (version.equals(id)) {
-                            versionUrl = new URL(versionObj.get("url").getAsString());
-                            break;
+                if (VERSIONS_ARRAY != null) {
+                    URL versionUrl = null;
+                    for (JsonElement versionElement : VERSIONS_ARRAY) {
+                        if (versionElement instanceof JsonObject) {
+                            JsonObject versionObj = versionElement.getAsJsonObject();
+                            String id = versionObj.get("id").getAsString();
+                            if (version.equals(id)) {
+                                versionUrl = new URL(versionObj.get("url").getAsString());
+                                break;
+                            }
+                        }
+                    }
+                    if (versionUrl == null) {
+                        AssetMoverAPI.LOGGER.fatal("Invalid version detetected, {} is not present in the manifest json", version);
+                    } else {
+                        long start = System.currentTimeMillis();
+                        HttpURLConnection con = (HttpURLConnection) versionUrl.openConnection();
+                        con.setRequestProperty("User-Agent", USER_AGENT);
+                        try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), Charsets.UTF_8))) {
+                            versionObject = new JsonParser().parse(br).getAsJsonObject();
+                            AssetMoverAPI.LOGGER.info("Manifest of {} downloaded in {}", version, getTime(System.currentTimeMillis() - start));
+                        } catch (IOException e) {
+                            AssetMoverAPI.LOGGER.fatal("Unexpected error occurred while downloading versions json", e);
                         }
                     }
                 }
-                if (versionUrl == null) {
-                    throw AssetMoverAPI.LOGGER.throwing(new IllegalArgumentException("Invalid version detected, " + version + " is not present in manifest json"));
-                }
-                long start = System.currentTimeMillis();
-                HttpURLConnection con = (HttpURLConnection) versionUrl.openConnection();
-                con.setRequestProperty("User-Agent", USER_AGENT);
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), Charsets.UTF_8))) {
-                    versionObject = new JsonParser().parse(br).getAsJsonObject();
-                    AssetMoverAPI.LOGGER.info("Manifest of {} downloaded in {}", version, getTime(System.currentTimeMillis() - start));
-                } catch (IOException e) {
-                    AssetMoverAPI.LOGGER.fatal("Unexpected error occurred while downloading versions json", e);
-                }
             }
-            info = new VersionAssetsInfo();
-            info.versionJson = versionObject;
-            VERSION_ASSET_INFO.put(version, info);
+            if (versionObject != null) {
+                info = new VersionAssetsInfo();
+                info.versionJson = versionObject;
+                VERSION_ASSET_INFO.put(version, info);
+            }
         }
         return versionObject;
     }
@@ -208,7 +215,7 @@ public enum AssetMoverHelper {
         }
         if (EXECUTOR == null) {
             AssetMoverAPI.LOGGER.info("Building Executor...");
-            EXECUTOR = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setPriority(3).setNameFormat("AssetMover Executor #%d").setDaemon(true).build());
+            EXECUTOR = Executors.newSingleThreadExecutor(new ThreadFactoryBuilder().setPriority(3).setNameFormat("AssetMover Downloader #%d").setDaemon(true).build());
         }
         final Map<String, String> finalizedObjects = objects;
         final VersionAssetsInfo finalizedInfo = info;
